6842a3078572d4886ebebe4160ac5239290a2071
Levon <levonghukasy@gmail.com>
2020-01-10 10:57:34 +0400

    add all tasks

---
+++ index.js
@@ -0,0 +1,317 @@
+// console.log(a);
+// var a = 15;
+// console.log(a);
+
+
+// console.log(a);
+// let a = 15;
+// console.log(a);
+
+
+// var a = 5;
+// var a = 8;
+// console.log(a);
+
+
+// let a = 5;
+// let a = 8;
+// console.log(a);
+
+
+// let a = 5;
+// a = 8;
+// console.log(a);
+
+
+// const A = 15;
+// console.log(A);
+// var u;
+// var i;
+// const P;
+// console.log(u);
+// console.log(i);
+// console.log(P);
+
+
+// let a;
+// a = 15;
+// console.log(a);
+
+
+// let a;
+// a = function () {
+// };
+// console.log(a);
+
+
+// function a(){
+// };
+// var a = 15;
+// console.log(a);
+
+
+// function a(){
+// }
+// let a = 15;
+// console.log(a);
+
+
+// function a(){
+// }
+// a = 15;
+// console.log(a);
+
+
+// function a(){
+//     console.log("Hello")
+// };
+// console.log(a);
+// a();
+// a=15;
+// console.log(a);
+
+
+// function a(){
+//     console.log("Hello")
+// };
+// console.log(a);
+// a();
+// a= 3;
+// console.log(a);
+// a = function(){
+//     console.log("Hello1")
+// };
+// a();
+
+
+// a();
+// console.log(a);
+// function a(){
+//     console.log("Hello");
+// };
+// a = 5;
+// console.log(a);
+
+// console.log(b);
+// console.log(a);
+// function b(){
+// }
+// var a = 15;
+// console.log(a);
+// console.log(b);
+
+
+// {
+//     var a = 15
+// };
+// console.log(a);
+
+
+// {
+//     let a = 15
+// };
+// console.log(a);
+
+
+// let a = 15;
+// function f() {
+//     console.log(a);
+// };
+// f();
+// a = 5;
+// f();
+
+
+// let a = 15;
+// function f(a) {
+//     console.log(a);
+// };
+// f();
+// a = 5;
+// f();
+
+
+// let a = 15;
+// function f(a) {
+//     console.log(a);
+// };
+// f(1);
+// a = 5;
+// f(10);
+
+
+// let a = 10;
+// function f(a){
+//     console.log(a);
+// };
+// function f1(){
+//     console.log(a)
+// };
+// f();
+// f1();
+
+
+// let a = 10;
+// function f1(a){
+//     console.log(a);
+//     f2()
+// };
+// function f2(){
+//     console.log(a)
+// };
+// f1(1);
+// f2();
+
+
+// let a = 10;
+// function f1(a){
+//     function f2(){
+//         console.log(a)
+//     }
+//     console.log(a);
+//     f2()
+// };
+// function f2(){
+//     console.log(a)
+// };
+// f1(1);
+// f2();
+
+
+// function f(){
+//     console.log("Hello1")
+// };
+// f();
+// function f(){
+//     console.log("Hello2")
+// };
+// f();
+
+
+// function f(){
+
+// };
+// let a = f;
+// let b  = f();
+// console.log(a,b);
+
+
+// function f(){
+//     return
+// };
+// function f1(){
+
+// };
+// function f2(){
+//     return undefined
+// };
+// let a = f();
+// let b = f1();
+// let c = f2();
+// console.log(a,b,c);
+
+
+// function f(){
+//     return 5;
+// };
+// function f1(a){
+//     return a
+// };
+// let r = f();
+// let c = f1();
+// let y = f1(1);
+// let p = f1(5);
+// console.log(r,c,y,p);
+
+
+// let r = function(){
+//     console.log("Hello1")
+// };
+// function f(){
+//     return function(){
+//         console.log("Hello")
+//     }
+// };
+// let u = f();
+// console.log(r,u);
+// r();
+// u();
+
+
+// function f() {
+//     return "Hello"
+// };
+// function f1() {
+//     return 15
+// };
+// console.log(f() + f1());
+
+
+// function f(a) {
+//     return a
+// };
+// let u = f(5);
+// console.log(u);
+// let t = f(true);
+// console.log(t);
+// let y = f("Hello");
+// console.log(y);
+// let g = f([1, 2, 3]);
+// console.log(g);
+// let r = f(function () {
+// });
+// console.log(r);
+// let k = f(function () {
+//     console.log("Hello")
+// });
+// console.log(k);
+// k();
+// let m = f(function(){
+//     return 10
+// });
+// console.log(m);
+// m();
+// let h = m();
+// console.log(h,m());
+
+
+// function g(){
+//     console.log("Hello1")
+// };
+// let p = function(){
+//     console.log("hello2")
+// };
+// console.log(g,p);
+// g();
+// p();
+
+
+// let o = function(){
+//     console.log("Hello1")
+// };
+// let r = ()=>{
+//     console.log("Hello2")
+// };
+// console.log(o,r);
+// o();
+// r();
+
+
+// let r = (a) => {
+//     return a
+// };
+// let u = a => {
+//     return a
+// };
+// let m = (a) => {
+//     a
+// };
+// let l = a => {
+//     a
+// };
+// let e = (a) => a;
+// let o = a => a;
+// console.log(r,u,m,l,e,o);
+// console.log(r(),u(),m(),l(),e(),o());
+// console.log(r(10),u(15),m(3),l(8),e(23),o(15));
+// console.log(r(10),u([]),m({}),l("Hello"),e(null),o(true));
