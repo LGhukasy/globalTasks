a77369bdd64de8d2c54a573a866e8fd903e937b2
Levon <levonghukasy@gmail.com>
2020-01-12 23:51:07 +0400

    add some tasks v.2

--- index.js
+++ index.js
@@ -360,7 +360,6 @@
 
 
 // let u = function(){
-
 // }();
 // console.log(u);
 // let r = function(){
@@ -395,3 +394,643 @@
 //     })();
 // };
 // f();
+
+
+// let k = (()=>5)();
+// console.log(k);
+
+
+// function f(a){
+//     return a;
+// };
+// console.log(f);
+// console.log(f(15));
+// console.log(f(true));
+// console.log(f("Hello"));
+// console.log(f(function(){return 10})());
+
+
+// let r = 10;
+// function f(a,b,cb){
+//     let r = a+b;
+//     cb(r);
+//     function f2(){
+//         console.log(r)
+//     };
+//     f2();
+//     f1();
+// };
+// function f1(){
+//     console.log(r)
+// };
+// f(1,2,function(y){
+//     console.log(++y)
+// });
+// function f(){
+//     this.name = "Levon"
+// };
+// let obj1 = {
+//     lastName : "AAA"
+// };
+// let obj = {
+//     surname : "Ghukasyan",
+//     __proto__ : obj1,
+// };
+// f.prototype = obj;
+// let k = new f();
+// console.log(k.lastName);
+// console.log(a);
+// console.log(u);
+// var a = 15;
+// function u(){
+
+// }
+// let name = 15;
+// let Name = 10;
+// console.log(name,Name);
+// let a = 15;
+// console.log(a);
+// [1,2,3]
+// function a(){
+
+// };
+// var a = 15;
+// console.log(a);  
+// var a = 15;
+// let a = 5;
+// let b = 45;
+// a = b++;
+// console.log(a,b);
+// console.log(++a + b++ - a++ - b++)
+
+// function f(...h){
+//     console.log(h)
+// };
+// f(1,23,4,65)
+
+// function f(cb){
+// if(cb){
+//     cb()
+// }
+// else{
+//     console.log("aaa")
+// }
+// };
+// f(()=>console.log("b"))
+
+// function f(cb){
+//     cb ? cb():console.log("aaa");
+// };
+// f();
+
+// function f(cb) {
+//     (cb || console.log("aaa")) && cb()
+// };
+// f(() => { console.log("PP") });
+// let b;
+// b = !!0 && 5 || !{} && 10 ||15;
+// console.log(b)
+// let a = 5;
+// let b = 10;
+// function f(cb,cb1){
+//     cb(cb1)
+// };
+// function f1(){
+//     console.log(a+b)
+// };
+// f(function(cb){
+//     cb()
+// },f1);
+
+// function f(...a){
+//     console.log(a);
+//     console.log(a[1]());
+//     a[3]();
+// };
+// f(15,()=>{},function(){},function(){console.log("Hello1")},(function(){console.log("Hello")})(),(()=>5)())
+
+// let a = {
+//     "name" : "Levon",
+//     surname : "Ghukasyan",
+//     "sdhyasid sdbasib" : 458,
+//     kkk : undefined,
+//     sdbasyd : true,
+//     "hhh" : 777,
+// };
+// console.log(a);
+// a.somethig = function(){};
+// console.log(a);
+// console.log(a.name);
+// a.name = "ksdasid";
+// console.log(a);
+// a["sydaysd sidbasid sdabs"] = 15;
+// console.log(a);
+// console.log(a["surname"]);
+// console.log(a["sdhyasid sdbasib"]);
+// console.log(a.hhh)
+// let a = 15;
+// console.log(global.a)
+// let a = 15;
+// let b = "leet";
+// let obj = {
+//     a,
+//     b : a,
+//     let : 789
+// };
+// obj.let = 54;
+// console.log(obj);
+// console.log("let" in obj);
+
+// for(a in obj){
+//     console.log(obj[a]);
+// }
+// let a = ["Hello",{},function(){}];
+// console.log(a["2"]);
+// obj = {
+//     "+4" : "Levon",
+//     "55" : "Ghuaksyan",
+//     "1" : 24,
+//     "hello" : true,
+// };
+// console.log(obj);
+// for(o in obj){
+//     console.log(o);
+//     console.log(obj[o]);
+// }
+
+// let r = "Hello1";
+// let p = r;
+// let u = r;
+// console.log(r,p,u);
+// let r = "Hello1";
+// let p = r;
+// let u = p;
+// u = "Hello2";
+// console.log(r,p,u);
+// let r = {
+//     name : "Hello"
+// };
+// let p = {
+//     name : "Hello3"
+// };
+// let u = p;
+// console.log(r,p,u);
+// u.name = "Hello1";
+// console.log(r,p,u);
+
+
+// let b = {
+//     name : "Gor",
+//     surname : "Hovakimyan",
+// };
+// let a = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+//     friend : b,
+// };
+// b.name = "GOR";
+// b = 15;
+// b = a.friend;
+// console.log(a,b);
+
+// let a = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+// };
+// console.log(a);
+// delete a.surname;
+// console.log(a);
+// let b = {
+//     c : "Hello"
+// };
+// let a = {
+//     a : 15,
+//     b : b,
+// };
+// b.c = "Hello15";
+// b = 1556;
+// let c = a.b;
+// delete a.b;
+// console.log(b);
+// console.log(c)
+
+// function f(){
+//     console.log(this)
+// };
+// f();
+// console.log(global)
+
+// let a = {
+//     a : "Hello",
+//     b : function(){
+//         console.log(this)
+//     },
+// };
+// let r = a.b;
+
+// let p = a.b;
+
+// let o = {
+//     y : "suihduisa",
+//     l : p,
+// };
+
+// let kjk = a;
+// console.log(kjk);
+// kjk.lll = a.b;
+// kjk.lll();
+
+// let y = {
+//     name : "Levon",
+//     tt : function(){
+//         console.log(this);
+//         let o = ()=>{
+//             console.log(this)
+//         };
+//         o();
+//     },
+//     lkl: ()=>{
+//         console.log(this)
+//     },
+// };
+// let u = y.tt;
+// u();
+// y.tt();
+// y.lkl()
+
+
+// let o = {
+
+// };
+// let r = function(){
+//     console.log(this)
+// };
+// o.f = r;
+// r();
+// o.f();
+// let a = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+//     male : true,
+//     run : function(){
+//         console.log("RUN")
+//     },
+// }
+
+// let a = 25;
+// let b = a;
+// console.log(b);
+
+// let c = {
+//     name : "Gor"
+// };
+// let u = c;
+// c.name = "GOR";
+// console.log(c);
+// console.log(u);
+// let a = {
+//     name : "Levon"
+// };
+// let b = {
+//     name : "Levon"
+// };
+// let c = b;
+// b.name = "HHH";
+// console.log(a,b,c);
+
+// let a = 15;
+
+// function f(){
+//     let a = 55;
+// };
+// f();
+
+// let a = {
+
+// };
+// a = "Hello";
+// let b = a;
+
+// let a = {
+//     name : "Levon"
+// };
+// console.log(a);
+// a.kjkjk = "suidisau";
+// console.log(a);
+
+// function f(){
+//     console.log(this);
+// };
+// f();
+
+// let a = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+//     writeThis : function(){
+//         console.log(this)
+//     } 
+// };
+// console.log(a);
+// let u = a.writeThis;
+// u();
+// a.writeThis();
+
+// let f = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+//     displayThis : function(){
+//         console.log(this)
+//     },
+// };
+// let g = {
+//     ksks : "HHH",
+//     ghj : f.displayThis
+// };
+// let o = g.ghj;
+// f.displayThis();
+// g.ghj();
+// o();
+
+// let obj = {
+//     name : "Levon",
+//     displayName : function(){
+//         console.log(this.name)
+//     },
+//     changeName : function(newName){
+//         this.name = newName
+//     }
+// };
+// let obj1 = {
+//     name : "HHH"
+// };
+// obj.displayName();
+// obj.changeName("Gor");
+// obj.displayName();
+// obj1.change = obj.changeName;
+// obj1.display = obj.displayName;
+// obj1.display();
+// obj1.change("Tatev");
+// obj1.display();
+
+
+// let obj = {
+//     func: function (...ags) {
+//         for (let i = 0; i < ags.length; i = i + 2) {
+//             if (typeof ags[i] === "string") {
+//                 this[ags[i]] = undefined
+//             }
+//             else {
+//                 this[String(ags[i])] = 7
+//             }
+//         }
+//         for (let j = 1; j < ags.length; j = j + 2) {
+//             this[ags[j - 1]] = ags[j]
+//         }
+//     }
+// };
+// obj.func("name","Levon","surname","Ghukasyan","age",24,214151);
+// console.log(obj);
+
+// function f5(){
+//     console.log(this)
+// };
+// let obj = {
+//     name : "Levon",
+//     f : function(){
+//         function f1(){
+//             console.log(this)
+//         };
+//         console.log(this);
+//         f1();
+//         f5();
+//     }
+// };
+// obj.f();
+
+// let obj = {
+//     name : "Levon",
+//     f : function(){
+//         console.log(this);
+//         function f1(a){
+//             a.surname = "Ghukasyan"
+//         };
+//         f1(this);
+//     }
+// };
+// obj.f();
+// console.log(obj.surname);
+
+
+// let obj = {
+//     name : "Gor",
+//     f : function(){
+//         console.log(this)
+//     },
+//     objkey : {
+//         surname : "Hovaimyan",
+//         f1 : function(){
+//             console.log(this)
+//         }
+//     }
+// };
+// obj.f();
+// obj.objkey.f1();
+
+// let obj = {
+//     name : "Levon",
+//     f : function(){
+//         console.log(this)
+//     }
+// };
+// (obj.f || "Hello")()
+
+// function f(a,b){
+//     return {
+//         name : a,
+//         surname : b,
+//     }
+// };
+// let obj = f("Levon","Ghuaksyan");
+// console.log(obj);
+
+// function Func(name,surname){
+//     this.name = name;
+//     this.surname = surname;
+    
+// };
+// let obj = new Func("Levon","Ghukasyan");
+// console.log(obj);
+
+// function f(g){
+//     function f1(...c){
+//         console.log(c)
+//     };
+//     f1(...g)
+// };
+// f([1,2,3,4]);
+
+// function f(){
+//     f.i ++ ;
+//     console.log(f.i);
+// };
+// f.i = 0;
+// f();
+// f();
+// f();
+// f();
+
+// let obj = {
+//     name : "Gor",
+//     surname : "Hovakimyan"
+// };
+// let obj1 = {
+
+// }
+// // function f(a,b,c){
+// //     console.log(this);
+// //     console.log(b);
+// // };
+// // f.call(obj,1,2,3);
+// function f(){
+//     this.name = "GOR";
+//     this.sydgsay = "saudasihdui"
+// };
+// console.log(obj);
+// f.call(obj);
+// f.call(obj1);
+// console.log(obj);
+// console.log(obj1);
+
+
+// let obj = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+//     f : function(a,b,c){
+//         console.log(this);
+//         console.log(a,b,c)
+//     },
+// };
+// let obj1 = {
+//     name : "Gor"
+// };
+// obj.f();
+// obj.f.call(obj1,1,2,3);
+// obj.f.apply(obj1,[1,2,3]);
+
+// let obj = {
+//     name : "Levon",
+//     surname : "Ghukasyan",
+// };
+// let obj1 = {
+//     name : "Gor",
+//     surname : "Hovakimyan",
+// }
+// let f = function(){
+//     console.log(this)
+// };
+// let f1 = function (){
+//     this.name = "YUSYYSHDY"
+// };
+// function wrapper(cb,ob){
+//     return function(){
+//         cb.call(ob)
+//     }
+// };
+// let hhh = wrapper(f,obj);
+// let ggg = wrapper(f,obj1);
+// let nnn = wrapper(f1,obj);
+// ggg();
+// hhh();
+// hhh();
+// nnn();
+// hhh();
+
+// let obj = {
+//     name : "Levon"
+// };
+// function f(){
+//     console.log(this)
+// };
+// let h = f.bind(obj);
+// h();
+
+// let obj2 = {
+//     age : 25,
+//     func : function(){
+//         console.log(this)
+//     }
+// };
+// let obj1 = {
+//     surname : "Ghukasyan",
+// };
+// let obj = {
+//     name : "Levon",
+//     __proto__ : obj1,
+// };
+// obj1.__proto__ = obj2;
+// console.log(obj.age);
+// obj.func();
+// obj1.func();
+// obj2.func();
+
+
+// let obj1 = {
+//     surname : "Ghukasyan",
+// };
+
+// let obj = {
+//     name : "Levon",
+//     surname : "sdasii",
+//     ghy : {
+//         surname : "Hovakimyan",
+//         __proto__ : obj1
+//     }
+// };
+
+// let obj2 = {
+//     __proto__ : obj,
+// };
+
+// console.log(obj.ghy.surname);
+// console.log(obj2.ghy.surname);
+
+// function Constr(name,surname){
+//     this.name = name;
+//     this.surname = surname
+// };
+// let user = new Constr("Levon","Ghukasyan");
+// let user1 = new Constr("Gor","Hovkimyan");
+// console.log(user);
+// console.log(user1);
+
+// let obj = {
+
+// };
+// let obj = new Object();
+
+// console.log(obj);
+
+// let obj = {
+//     age : 25,
+// };
+
+// function Constr(name,surname){
+//     this.name = name;
+//     this.surname = surname
+// };
+// Constr.prototype = obj;
+// let user = new Constr("Levon","Ghukasyan");
+// let user1 = new Constr("Gor","Hovkimyan");
+// console.log(user.age);
+// console.log(user1.age);
+
+// Object.prototype.displayName = function(){
+//     console.log("Levon")
+// };
+
+
+// let obj = {
+
+// };
+// console.log(obj);
+// obj.displayName()
